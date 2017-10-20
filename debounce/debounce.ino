be784f7a8a3a2e4007794611dd50a8081fba15e9
pbishnupuri <pbishnupuri@URB218-05.seasad.wustl.edu>
2017-10-19 19:17:29 -0500

    whatisthis

---
+++ debounce/debounce.ino
@@ -0,0 +1,37 @@
+const int buttonPin = 2;
+
+volatile int buttonState = HIGH;             // the current reading from the input pin
+volatile int lastButtonState = LOW;   // the previous reading from the input pin
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+volatile unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;   
+
+
+void buttonPressed() {
+  int reading = digitalRead(buttonPin);
+  if((millis() - lastDebounceTime > debounceDelay) && (reading == HIGH)){
+    Serial.println("Interrupt");
+    lastDebounceTime = millis();
+  }
+  if(reading == LOW){
+    lastDebounceTime = millis();
+  }
+}
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  // Interrupts can happen on "edges" of signals.  
+  // Three edge types are supported: CHANGE, RISING, and FALLING 
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonPressed, CHANGE);
+  Serial.begin(9600);
+}
+
+void loop() {
+  for(int i=0;i<100;i++) {
+    Serial.println(i);
+    delay(1000);
+  }
+}
--- src/studio6/SerialComm.java
+++ src/studio6/SerialComm.java
@@ -27,8 +27,34 @@ public class SerialComm {
 	}
 		
 	// TODO: Add writeByte() method from Studio 5
+	void writeByte(byte singleByte) {
+		try {
+			debug = port.writeByte(singleByte);
+		}
+		catch(SerialPortException e) {
+			e.printStackTrace();
+		}
+		if(debug) {
+			String temp = Integer.toHexString(singleByte);
+			System.out.println("0x" + temp);
+		}
+	}
+	
+	
 	
 	// TODO: Add available() method
+	boolean available() {
+		boolean avail = false;
+		try {
+			if(port.getInputBufferBytesCount() > 0)
+				avail = true;
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return avail;
+		
+	}
 	
 	// TODO: Add readByte() method	
 	
